4ff03ff6a3565620d2c89b7e5e210a026e12d18a
microz <speedpoiuytr@gmail.com>
2013-04-23 00:43:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import mimodulo
+import guiatv
+
+import guiatv
+guiatv.opciones()
+numero = input('')
+print('elegiste la opción',numero)
+if numero == '1':
+    print('¿cuantos canales deseas listar?')
+    cantidad = input('')
+    aenterocantidad = int(cantidadentero)
+    guiatv.listar((cantidadentero+1))
+    
+elif numero == '2':
+    guiatv.agregar()
+elif numero == '3':
+    guiatv.buscar()
+else:
+    guiatv.supererror()
+
+#THE-END
